1e91e853b836ccabd2b92d4f92eae3c3c8fe5fcb
Carlos Ruiz <carg67@gmail.com>
2024-09-09 15:37:08 +0200

    IDEMPIERE-2981 Implement JSON Field type + IDEMPIERE-6227 - fixes (#2449)
    * IDEMPIERE-2981 Implement JSON Field type
    
    - Fix the JSON Editor, is sending to the database the value non prettified
    
    * - Fix issue adding a JSON column with log migration script enabled
    
    When logging migration script the Convert layer is applied, and we didn't implement Convert for the JSON column
    
    * IDEMPIERE-6227 Cannot add a column with spaces on the default value (Convert layer logging migration script)

--- org.compiere.db.postgresql.provider/src/org/compiere/dbPort/Convert_PostgreSQL.java
+++ org.compiere.db.postgresql.provider/src/org/compiere/dbPort/Convert_PostgreSQL.java
@@ -101,6 +101,7 @@ public class Convert_PostgreSQL extends Convert_SQL92 {
 
 		} else {
 
+			statement = convertAddJson(statement);
 			statement = convertWithConvertMap(statement);
 			statement = convertSimilarTo(statement);
 			statement = DB_PostgreSQL.removeNativeKeyworkMarker(statement);
@@ -1094,10 +1095,16 @@ public class Convert_PostgreSQL extends Convert_SQL92 {
 						begin_default = rest.toUpperCase().indexOf(
 								" DEFAULT ") + 9;
 						defaultvalue = rest.substring(begin_default);
-						int nextspace = defaultvalue.indexOf(' ');
-						if (nextspace > -1) {
-						    rest = defaultvalue.substring(nextspace);
-						    defaultvalue = defaultvalue.substring(0, defaultvalue.indexOf(' '));
+						String endDefaultChar = " ";
+						int shift = 0;
+						if (defaultvalue.startsWith("'")) {
+							endDefaultChar = "'";
+							shift = 1;
+						}
+						int endDefault = defaultvalue.substring(shift).indexOf(endDefaultChar) + shift;
+						if (endDefault > -1+shift) {
+						    rest = defaultvalue.substring(endDefault+shift);
+						    defaultvalue = defaultvalue.substring(0, endDefault+shift);
 						} else {
 							rest = "";
 						}
@@ -1156,10 +1163,16 @@ public class Convert_PostgreSQL extends Convert_SQL92 {
 						begin_default = rest.toUpperCase().indexOf(
 								" DEFAULT ") + 9;
 						defaultvalue = rest.substring(begin_default);
-						int nextspace = defaultvalue.indexOf(' ');
-						if (nextspace > -1) {
-						    rest = defaultvalue.substring(nextspace);
-						    defaultvalue = defaultvalue.substring(0, defaultvalue.indexOf(' '));
+						String endDefaultChar = " ";
+						int shift = 0;
+						if (defaultvalue.startsWith("'")) {
+							endDefaultChar = "'";
+							shift = 1;
+						}
+						int endDefault = defaultvalue.substring(shift).indexOf(endDefaultChar) + shift;
+						if (endDefault > -1+shift) {
+						    rest = defaultvalue.substring(endDefault+shift);
+						    defaultvalue = defaultvalue.substring(0, endDefault+shift);
 						} else {
 							rest = "";
 						}
@@ -1209,4 +1222,21 @@ public class Convert_PostgreSQL extends Convert_SQL92 {
 
 		return sqlStatement;
 	}
+
+	/**
+	 * For JSON columns Oracle uses CLOB ... CONSTRAINT ... CHECK IS JSON
+	 * while oracle uses JSONB, no constraint
+	 * @param statement
+	 * @return
+	 */
+	private String convertAddJson(String statement) {
+		if (statement.toUpperCase().matches(".*\\bCLOB\\b.*\\bCONSTRAINT\\b.*CHECK\\b.*\\bIS JSON\\).*")) {
+			// remove the CONSTRAINT ... IS JSON part
+			statement = statement.replaceAll("(?i)\\bCONSTRAINT\\b.*CHECK\\b.*\\(.*\\bIS JSON\\)", "");
+			// change type CLOB to JSONB
+			statement = statement.replaceAll("(?i)\\bCLOB\\b", "JSONB");
+		}
+		return statement;
+	}
+
 } // Convert
